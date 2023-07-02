ef2b63900fd6132be247abec0499ba2048f0f925
Joseph Smith <joesmith40@gmail.com>
2023-07-02 15:47:26 -0500

    initial commit, created tables in models

--- server/app.py
+++ server/app.py
@@ -3,14 +3,68 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, session, jsonify, abort
 from flask_restful import Resource
 
 # Local imports
-from config import app, db, api
-from models import User, Recipe
+from config import app, db, api, bcrypt
+from models import User, Butterfly, Plant, Tag
 
-# Views go here!
+class Butterflies(Resource):
+    def get(self):
+        butterfly_list = [b.to_dict() for b in Butterfly.query.all()]
+        response = make_response(
+            butterfly_list,
+            200,
+        )
+
+        return response
+
+    def post(self):
+        form_json = request.get_json()
+        try:
+            new_butterfly = Butterfly(
+                name=form_json['name'],
+                image=form_json['image'],
+                user_id=session['user_id']
+            )
+        except ValueError as e:
+            abort(422,e.args[0])
+
+        db.session.add(new_butterfly)
+        db.session.commit()
+
+        response_dict = new_butterfly.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+        return response
+api.add_resource(Butterflies, '/butterflies')
+
+class ButterflyTag(Resource):
+    def post(self, id):
+        json = request.get_json()
+        tag = Tag.query.filter_by(name=json['name']).first()
+        if not tag:
+            tag = Tag(
+                name=json['name']
+            )
+        butterfly = Butterfly.query.filter_by(id=id).first()
+        butterfly.tags.append(tag)
+        db.session.commit()
+
+        response = make_response(
+            butterfly.to_dict(),
+            201
+        )
+        return response
+
+api.add_resource(ButterflyTag, '/butterflies/<int:id>/tag')
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,83 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
-# Models go here!
+butterfly_tags = db.Table('butterfly_tags',
+    db.Column('butterfly_id', db.Integer, db.ForeignKey('butterflies.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+)
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__= 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    admin = db.Column(db.String, default=False)
+    butterflies = db.relationship('Butterfly', backref='user')
+    plants = db.relationship('Plant', backref='user')
+
+    serialize_rules = ('-butterflies.user',)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('email')
+    def validate_email(self, key, email):
+        if '@' not in email:
+            raise ValueError("Please provide a valid email")
+        return email
+    
+    def __repr__(self):
+        return f'<Name:{self.username}, Email:{self.email}>'
+    
+
+class Butterfly(db.Model, SerializerMixin):
+    __tablename__ = 'butterflies'
+     
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    tags = db.relationship('Tag', secondary=butterfly_tags, back_populates='butterflies')
+    
+
+    def __repr__(self):
+        return f'<Name:{self.name}>'
+
+class Plant(db.Model, SerializerMixin):
+    __tablename__ = 'plants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    genus_species = db.Column(db.String, nullable=False)
+    growing_zone = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+
+    def __repr__(self):
+        return f'<Name:{self.name}, Genus/Species:{self.genus_species}, Growing Zone:{self.growing_zone}>'
+    
+class Tag(db.Model, SerializerMixin):
+    __tablename__= 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    butterflies = db.relationship('Butterfly', secondary=butterfly_tags, back_populates='tags')
+
+    def __repr__(self):
+        return f'<Colors:{self.colors}, Primary Food:{self.primary_food}>'
+
